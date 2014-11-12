a75881ac900be22e7832d163f870ef3f35c35857
os6sense <leej@sowhatresearch.com>
2014-11-12 16:34:10 +0000

    Fixed bug with 2 copies of mpv being started

--- recording-ui/src/pyqt_mpv_widget/MPVRunner.py
+++ recording-ui/src/pyqt_mpv_widget/MPVRunner.py
@@ -19,29 +19,32 @@ class MPVRunner(object):
     @staticmethod
     def execute(config):
         MPVRunner.LASTCONFIG = config
-        command = MPVRunner.CMD % config
+        MPVRunner.COMMAND = MPVRunner.CMD % config
         MPVRunner.STOP = False
         """
-        Run a process in the background.
         """
         if MPVRunner._monitor_thread == None:
             MPVRunner._monitor_thread = threading.Thread(target=MPVRunner.monitor, args=())
-            MPVRunner._monitor_thread.daemon = True 
-            MPVRunner._monitor_thread.start() 
+            MPVRunner._monitor_thread.daemon = True
+            MPVRunner._monitor_thread.start()
 
-        proc = subprocess.Popen(command,
-			       shell=True,
-			       universal_newlines=True,
-                               stdout=subprocess.PIPE,
-                               stdin=subprocess.PIPE,
-			       )
-        return proc
+    @staticmethod
+    def _execute(last_config):
+        MPVRunner.LASTCONFIG = last_config
+        return subprocess.Popen(MPVRunner.COMMAND,
+			        shell=True,
+			        universal_newlines=True,
+                                stdout=subprocess.PIPE,
+                                stdin=subprocess.PIPE,)
 
     @staticmethod
     def nuke():
-        for proc in psutil.process_iter():
-            if proc.name == MPVRunner.PROCNAME:
-                proc.kill()
+        try:
+            for proc in psutil.process_iter():
+                if proc.name() == MPVRunner.PROCNAME:
+                    proc.kill()
+        except:
+            pass
 
     @staticmethod
     def monitor():
@@ -49,13 +52,13 @@ class MPVRunner(object):
             found = False
 
             for proc in psutil.process_iter():
-                if proc.name == MPVRunner.PROCNAME:
+                if proc.name() == MPVRunner.PROCNAME:
                     found = True
 
             if not found:
                 if MPVRunner.LASTCONFIG != '':
-                    MPVRunner.execute(MPVRunner.LASTCONFIG)
+                    MPVRunner._execute(MPVRunner.LASTCONFIG)
 
             time.sleep(1)
 
-        thread.stop()
+        #MPVRunner._monitor_thread.stop()
